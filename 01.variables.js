6042ec624b7f5b0d18cfd92c9a4ac0c5906798b3
Harshadkasani <harshakasani@gmail.com>
2026-06-01 15:42:07 +0530

    first-day

---
+++ 01.variables.js
@@ -0,0 +1,63 @@
+// console.log('Hello world');
+// console.error('fetch error');
+// console.warn('fetch warning');
+
+
+// console.log('Harshad');
+// console.log(25);
+
+// const course = 'Electronics and Communication Engineering'
+// console.log(course);
+// var college = 'Sasi';
+// console.log(college);
+// college = 'Pragati'
+// console.log(college);
+// var college = 'High School';
+// console.log(college);
+// /*const course = 'MPC'*/
+// /*console.log(course);*/
+// let name = 'kkkk'
+// console.log(name);
+// name = 'sai'
+// console.log(name);
+// let name = 'Harshad'
+// console.log(name)
+
+//       /* 'let' keyword is used for re-assign*/
+// let a = 10;
+// console.log(a);
+// /*let a = 30;*/
+// a = 20;
+// console.log(a);
+
+//           /* 'const keyword is not used for re-declare and re-assign*/
+// const b = 30;
+// console.log(b)
+
+// var c = 'Harshad'
+// console.log(c)
+// var c = '25'
+// console.log(c)
+// c = 'redeclar'
+// console.log(c)
+
+let studentName = 'Harshad';
+let age = 25;
+let course = 'ECE';
+let college = 'Sasi';
+let skills = 'HTML,CSS,JavaScript';
+
+
+
+console.log(studentName);
+console.log(age);
+console.log(course);
+console.log(college);
+console.log(skills);
+
+console.log(typeof studentName);
+console.log(typeof age);
+
+let isOnline = false;
+console.log(isOnline);
+console.log(typeof isOnline);
