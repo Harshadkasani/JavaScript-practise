f344e56935dd4586d7242e408b33c9a81a6c4e58
Harshadkasani <harshakasani@gmail.com>
2026-06-02 22:10:09 +0530

    day3-practise

---
+++ day3.js
@@ -0,0 +1,145 @@
+/* for loop*/
+/*print 1 to 10 numbers*/
+
+// for (let i=1;i<=10;i++){
+//     console.log(i)
+// }
+
+// /*print even numbers upto 10*/
+
+// for (let i=2;i<=10;i+=2){
+//     console.log(i)
+// }
+
+// /*print odd numbers upto 10*/
+
+// for (let i=1;i<=10;i+=2){
+//       console.log(i)
+
+// }
+//  /* print reverse numbers upto 0*/
+
+// for (i=10;i>=0;i--){
+//     console.log(i)
+// }
+
+// /* print 1 to 10 numbers condition after 5 we will exit the loop*/
+
+// // for (let i=1;i<=10;i++){
+// //     console.log(i)
+// //     if (i==5){
+// //         break;
+// //     }
+// // }
+
+// for (let i=1;i<=10;i++){
+    
+//     if (i==6){
+//         break;
+//     }
+//     console.log(i)
+
+// }
+
+// for (let i=1;i<=10;i++){
+    
+//     if (i==6){
+//         break;
+//     }
+//     console.log(i)
+
+// }
+// /*print 1 to 10 numbers expect 6 using continue*/
+
+// for (let i=1;i<=10;i++){
+    
+    
+//     if (i==6){
+//         continue;
+//     }
+//     console.log(i)
+// }
+
+/*print 1 to 10 numbers using while loop */
+// let i = 1;
+// while (i<=10){
+    
+//     console.log(i)
+//     i+=1
+// }
+
+/* print even numbers using while loop*/
+// let i = 2;
+// while (i<=10){
+    
+//     console.log(i)
+//     i+=2
+// }
+
+/* print odd numbers using while loop*/
+// let i = 1;
+// while (i<=10){
+    
+//     console.log(i)
+//     i+=2
+// }
+
+/*print 1 t0 10 numbers after 5 loop will break*/
+
+// let i = 1;
+// while (i<=10){
+//     if (i==6){
+//         break;
+//     }
+    
+//     console.log(i)
+//     i+=1
+// }
+
+/*print 1 to 10 numbers expect 6 using continue*/
+
+// let i = 1;
+// while (i<=10){
+//     if (i==6){
+//         i+=1;
+//         continue;
+//     }
+//     console.log(i)
+//     i+=1
+// }
+
+/* do while loop .this loop executes once even codition is False*/
+// let i = 23;
+// do{
+//     console.log('this is do while loop')
+//     i+=1
+
+// }while(i<20)
+
+let i = 1;
+do{
+    console.log('this is do while loop')
+    i+=1
+
+}while(i<20)
+
+/*print 5 multiples using for loop*/
+
+for (let i=5;i<=50;i+=5){
+    console.log(i)
+
+}
+
+// let number = 5;
+// for (let i=1;i<=10;i+=1){
+//     console.log(number*i)
+
+// }
+
+/*print five table using for loop*/
+
+let number = 5;
+for (let i=1;i<=10;i+=1){
+    console.log(`${number} x ${i} = ${number*i}`)
+
+}
