dad92f5691afdee16a6dd7577765e92b54bde208
Harshadkasani <harshakasani@gmail.com>
2026-06-16 16:28:44 +0530

    second commit

--- DOM_p1.js
+++ DOM_p1.js
@@ -19,7 +19,7 @@
 
               /* JAVASCRIPT CODE*/
 
-const button = document.querySelector('.btn')
+/*const button = document.querySelector('.btn')
 console.log(button)
 function clicked(){
     if (button.innerHTML === 'Subscribe'){
@@ -36,4 +36,42 @@ function clicked(){
 
     }
 }
+*/
 
+/*       HTML CODE
+<body>
+
+    <h1 id="heading1">My name is Harshad</h1>
+    <h1 id="heading2">My name is vinay</h1>
+
+    <p id="descript">I have done my Bachelors degree</p>
+    <button class="btn" onclick="clicked()">click</button>
+
+</body>*/
+
+     /* JAVASCRIPT */
+
+const heading = document.querySelector('h1')
+const paragraph = document.querySelector('p')
+
+function clicked(){
+    if (heading.innerHTML ==='My name is Harshad'){
+        heading.innerHTML = 'My name is Harshad Kasani' 
+        heading.style.color = 'blue'
+        paragraph.innerHTML = 'I have completed M.tech'
+        paragraph.style.color = 'green'
+
+
+    }
+}
+
+      /* Practise element by id*/
+
+const heading1 = document.getElementById('heading1')
+console.log(heading1)
+
+const paragraph = document.getElementById('descript')
+console.log(paragraph)
+
+const heading2 = document.getElementById('heading2')
+console.log(heading2)
